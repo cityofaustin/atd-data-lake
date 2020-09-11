ccb1f13883f2524979e2b292cac4a53a8dcdd48f
kperrine <kperrine@utexas.edu>
2020-09-11 14:30:11 -0500

    Need to sanitize slashes in street names for GS.

--- src/drivers/devices/gs_support.py
+++ src/drivers/devices/gs_support.py
@@ -72,6 +72,7 @@ def retrieveDevices(gsUnitData, devFilter=".*"):
         #if True: # TODO: It seems as though the "ip_comm_status" often says "OFFLINE" when the device is actually responding. 
         if row["device_status"].strip().upper() != "REMOVED" and row["atd_location_id"] and str(row["atd_location_id"]) != 'nan':
             streetName = row["primary_st"] + "_" + row["cross_st"]
+            streetName = streetName.replace("/", "&") # Needed to sanitize for filenames.
             if not regexp.search(streetName):
                 continue
             try:
